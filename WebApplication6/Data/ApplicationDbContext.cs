966ce84c33917c5e725bd7dc9e5f8f2d1d108525
uczen-admin <uczen-admin@DESKTOP-VI63AK0>
2023-11-13 09:11:27 +0100

    Dodaj pliki projektów.

---
+++ WebApplication6/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication6.Models;
+
+namespace WebApplication6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebApplication6.Models.Uczen>? Uczen { get; set; }
+    }
+}
---
+++ WebApplication6/Models/Uczen.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication6.Models
+{
+    public class Uczen
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surename { get; set; }
+        public string Ulica { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public string UczenUserId { get; set; }
+
+        public IdentityUser? UczenUser { get; set; }
+    }
+}
